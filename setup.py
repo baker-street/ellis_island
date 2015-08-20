b3091967619685f988057a1bec2504156f873dff
steven_c <steven.c.projects@gmail.com>
2015-08-20 02:34:46 -0500

    Added a README. Automated listing in setup.py.

--- setup.py
+++ setup.py
@@ -4,13 +4,9 @@ ellis_island  Copyright (C) 2015  Steven Cutting - License: ellis_island/LICENSE
 """
 from setuptools import setup, find_packages
 
-THE_LONG_DESCRIPTION = """
-Makes misc files usable for nlp, en masse.
-"""
+with open("README.md") as fp:
+    THE_LONG_DESCRIPTION = fp.read()
 
-PACKAGES = ['ellis_island',
-            'ellis_island.utils',
-            ]
 
 setup(
     name='ellis_island',
@@ -21,15 +17,16 @@ setup(
     classifiers=['Topic :: NLP',
                  'Topic :: Text cleaning',
                  'Intended Audience :: Developers',
+                 'Intended Audience :: Data Scientists',
                  'Operating System :: GNU Linux',
                  'Development Status :: 3 - Alpha',
                  'Programming Language :: Python :: 2.7',
-                 'License :: MIT License',
+                 'License :: GNU GPL v3+',
                  ],
     keywords='nlp encoding text plaintext preprocessing',
     author='Steven Cutting',
     author_email='steven.e.cutting@linux.com',
-    packages=PACKAGES,
+    packages=find_packages(exclude=('scripts', 'tests')),
     # zip_safe=False,
     install_requires=['pathlib',
                       'pyyaml',
@@ -39,40 +36,5 @@ setup(
                       'smart_open',  # TODO (steven_c) phase out smart_open
                       'boto3',
                       ],
-    scripts=['scripts/docker_script.py']
-)
-"""
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-
-'''setup(ext_modules=cythonize("ellis_island/utils/misc.pyx"),
-      ext_modules=cythonize("ellis_island/utils/parallel_easy.pyx"),
-      ext_modules=cythonize("ellis_island/utils/threading_easy.pyx"),
-      ext_modules=cythonize("ellis_island/fullprep.pyx"),
-      ext_modules=cythonize("ellis_island/prepforstash.pyx"),
-      ext_modules=cythonize("ellis_island/registrar.pyx"),
-      ext_modules=cythonize("ellis_island/stach.pyx"),
-      ext_modules=cythonize("ellis_island/stashtodatabase.pyx"),
-      ext_modules=cythonize("ellis_island/stashtofile.pyx"))
-'''
-
-extensions = [Extension('*', ['ellis_island/registrar.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=['gentrify'],
-                        # library_dirs=['/home/steven_c/projects/gentrify/gentrify']
-                        ),
-              Extension('*', ['ellis_island/*.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=['gentrify'],
-                        # library_dirs=['/home/steven_c/projects/gentrify/gentrify/']
-                        ),
-              Extension('*', ['ellis_island/utils/*.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=[...],
-                        # library_dirs=[...])
-                        )]
-setup(name="ellis_island",
-      ext_modules=cythonize(extensions),
-      )
-"""
+    scripts=['scripts/docker_script.py'],
+    )

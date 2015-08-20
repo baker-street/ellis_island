dcd905b084e9d4139889e5196ac71fe325aeed0f
steven_c <steven.c.projects@gmail.com>
2015-08-20 03:17:01 -0500

    fixed setup.py, rm text git added.

--- setup.py
+++ setup.py
@@ -36,45 +36,5 @@ setup(
                       'smart_open',  # TODO (steven_c) phase out smart_open
                       'boto3',
                       ],
-<<<<<<< HEAD
     scripts=['scripts/docker_script.py']
     )
-""" Cython Stuff
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-
-'''setup(ext_modules=cythonize("ellis_island/utils/misc.pyx"),
-      ext_modules=cythonize("ellis_island/utils/parallel_easy.pyx"),
-      ext_modules=cythonize("ellis_island/utils/threading_easy.pyx"),
-      ext_modules=cythonize("ellis_island/fullprep.pyx"),
-      ext_modules=cythonize("ellis_island/prepforstash.pyx"),
-      ext_modules=cythonize("ellis_island/registrar.pyx"),
-      ext_modules=cythonize("ellis_island/stach.pyx"),
-      ext_modules=cythonize("ellis_island/stashtodatabase.pyx"),
-      ext_modules=cythonize("ellis_island/stashtofile.pyx"))
-'''
-
-extensions = [Extension('*', ['ellis_island/registrar.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=['gentrify'],
-                        # library_dirs=['/home/steven_c/projects/gentrify/gentrify']
-                        ),
-              Extension('*', ['ellis_island/*.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=['gentrify'],
-                        # library_dirs=['/home/steven_c/projects/gentrify/gentrify/']
-                        ),
-              Extension('*', ['ellis_island/utils/*.pyx'],
-                        include_dirs=['ellis_island', 'ellis_island/utils'],
-                        # libraries=[...],
-                        # library_dirs=[...])
-                        )]
-setup(name="ellis_island",
-      ext_modules=cythonize(extensions),
-      )
-"""
-=======
-    scripts=['scripts/docker_script.py'],
-    )
->>>>>>> master

94e45a0e84d0dfea1b892b9acea68fd016cb67f4
steven_c <steven.c.projects@gmail.com>
2015-09-02 09:03:00 -0500

    ripped out io. put in superserial.

--- ellis_island/stashtodatabase.py
+++ ellis_island/stashtodatabase.py
@@ -9,9 +9,7 @@ __license__ = "GPL3"
 __maintainer__ = "Steven Cutting"
 __email__ = 'steven.c.projects@gmail.com'
 
-from os import getenv
 
-import dataset
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import(Column,
@@ -23,9 +21,6 @@ from sqlalchemy import(Column,
                        Date,
                        BLOB)
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy.exc import NoSuchTableError
-
-from ellis_island.utils.misc import get_default_data_key
 
 
 def default_create_table_sqlalchemy(uri, tablename='metatable'):
@@ -64,81 +59,3 @@ def default_create_table_sqlalchemy(uri, tablename='metatable'):
         text_checksum = Column(Text)
 
     Base.metadata.create_all(engine)
-
-
-class SQLStash(object):
-    def __init__(self, uri,
-                 table=getenv('METADATA_TABLE_NAME', 'metadata'),
-                 chuncksize=500,
-                 types={'uuid': String(36),  # TODO (steven_c)consider removeing
-                        'type': Text,
-                        'mime': Text,
-                        'info': Text,
-                        'generation': Integer,
-                        'parent': Text,
-                        'use': Boolean,
-                        'raw_pointer': Text,
-                        'text_pointer': Text,
-                        'org_filename': Text,
-                        },
-                 encryptkey=getenv('DAS_ENCRYPT_KEY', get_default_data_key()),
-                 # TODO (steven_c) consider removeing
-                 index=False, indexcolumns=[['uuid', 'type'],
-                                            ['uuid', 'generation'],
-                                            ['uuid', 'parent'],
-                                            ['uuid', 'use'],
-                                            ['uuid'],
-                                            ['type'],
-                                            ['parent'],
-                                            ['use'],
-                                            ['generation'],
-                                            ],
-                 **xargs):
-        self.uri = uri
-        self.chuncksize = chuncksize
-        self.table = table
-        self.types = types
-        self.index = index
-        self.indexcolumns = indexcolumns
-        self.conn = dataset.connect(uri, reflect_metadata=False,
-                                    engine_kwargs={'pool_recycle': 3600,
-                                                   'convert_unicode': True,
-                                                   'encoding': 'utf-8',
-                                                   })
-        self.conn.begin()
-        try:
-            self.tbl = self.conn.load_table(self.table)
-        except NoSuchTableError:
-            self.tbl = self.conn.get_table(self.table,
-                                           primary_id='uuid',
-                                           primary_type='String(36)')
-        if bool(self.index) and bool(self.indexcolumns):
-            if isinstance(self.indexcolumns[0], basestring):
-                self.tbl.create_index(columns=self.indexcolumns)
-            elif isinstance(self.indexcolumns[0], list):
-                for ic in self.indexcolumns:
-                    self.tbl.create_index(columns=ic)
-        self.stack = []
-
-    def flush_the_stack(self):
-        self.tbl.insert_many(rows=self.stack,
-                             chunk_size=self.chuncksize,
-                             types=self.types)
-        self.conn.commit()
-        self.conn.begin
-        self.stack = []
-
-    def stash(self, datumdict):
-        self.stack.append(datumdict)
-        if len(self.stack) >= self.chuncksize:
-            self.flush_the_stack()
-
-    def close(self):
-        self.flush_the_stack()
-        self.conn.commit()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
